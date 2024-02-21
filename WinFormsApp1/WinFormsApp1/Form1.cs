b80a2a6799a3ad70a71dc11a3eade67d289eeca7
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2024-02-21 08:06:49 +0700

    feat: update comment

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ namespace WinFormsApp1
     public partial class Form1 : Form
     {
         // cap nhat lan 1
+        //cap nhat nhanh con
         public Form1()
         {
             InitializeComponent();
